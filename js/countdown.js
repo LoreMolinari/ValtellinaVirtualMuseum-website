b9e20b498fcff5005698597bad3eeb77a48a64b7
LoreMolinari <lore.molinari.00@gmail.com>
2022-05-06 15:53:57 +0200

    Fixed some elements
    Fixed some elements

---
+++ js/countdown.js
@@ -0,0 +1,24 @@
+(function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    
+    const countDown = new Date("05/15/2022").getTime(),
+        x = setInterval(function() {    
+  
+          const now = new Date().getTime(),
+                distance = countDown - now;
+  
+            document.getElementById("days").innerText = Math.floor(distance / (day));
+  
+          //do something later when date is reached
+          if (distance < 0) {
+            document.getElementById("text").style.display = "block";
+            document.getElementById("countdown").style.display = "none";
+            document.getElementById("btn-segale").classList.remove("disabled");
+          }
+          //seconds
+        }, 0)
+    }());
